580682b68d0ffca3d8084073ed4f32bc3d37b432
sakayuka <skyk.dev@gmail.com>
2021-07-19 22:13:36 +0900

    add auth middleware

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,17 @@ module github.com/sakayuka/vue-go-firebase-handson/api
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.4.0
+require (
+	cloud.google.com/go v0.87.0 // indirect
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	cloud.google.com/go/storage v1.16.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo/v4 v4.4.0
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	google.golang.org/api v0.50.0 // indirect
+	google.golang.org/genproto v0.0.0-20210716133855-ce7ef5c701ea // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,27 +1,79 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
+	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"google.golang.org/api/option"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"http://localhost:8080"},
 		AllowCredentials: true,
 	}))
 	e.GET("/public", public)
-	e.GET("/private", private)
+	e.GET("/private", private, Auth)
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
 func public(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello Public World!")
+	return c.JSON(http.StatusOK, "Hello Public World!")
 }
 
 func private(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello Private World!")
+	return c.JSON(http.StatusOK, "Hello Private World!")
+}
+
+func Auth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		opt := option.WithCredentialsFile(os.Getenv("FIREBASE_SERVICE_ACCOUNT_KEY_JSON"))
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Println(err.Error())
+			return APIResponseError(c, http.StatusInternalServerError, "invalid credential file", errors.New("invalid credential file"))
+		}
+
+		client, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Println(err.Error())
+			return APIResponseError(c, http.StatusInternalServerError, err.Error(), err)
+		}
+
+		auth := c.Request().Header.Get("Authorization")
+		idToken := strings.Replace(auth, "Bearer ", "", 1)
+		token, err := client.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			fmt.Println(err.Error())
+			return APIResponseError(c, http.StatusUnauthorized, err.Error(), err)
+		}
+
+		c.Set("token", token)
+		return next(c)
+	}
+}
+
+type APIErrorResponse struct {
+	Code   string   `json:"code"`
+	Errors []string `json:"errors"`
+}
+
+func APIResponseError(c echo.Context, httpStatus int, message string, err error) error {
+	c.JSONPretty(httpStatus, APIErrorResponse{Code: fmt.Sprintf("%d-000", httpStatus), Errors: []string{message}}, "  ")
+	return err
 }
